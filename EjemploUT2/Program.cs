cfae4f09e6ca092bc51e7f2b4c23d49a433ee8fe
Ismael Umpierrez <IUmpierrez@infragistics.com>
2022-04-26 21:01:54 -0300

    Init

---
+++ EjemploUT2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjemploUT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
