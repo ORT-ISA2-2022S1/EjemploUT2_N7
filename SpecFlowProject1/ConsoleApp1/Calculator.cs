d4094ed2376d282b0a3567dde4f6be1af1887de2
Ismael Umpierrez <IUmpierrez@infragistics.com>
2022-05-17 21:18:34 -0300

    solucion specflow

---
+++ SpecFlowProject1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator 
+    {
+        public int AddTwoNumbers(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
---
+++ SpecFlowProject1/SpecFlowProject1/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp1;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        Calculator _calculator = new Calculator();
+        int i1;
+        int i2;
+        int res;
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            i1 = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            i2 = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            res = _calculator.AddTwoNumbers(i1, i2);
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.AreEqual(result, res);
+        }
+    }
+}
